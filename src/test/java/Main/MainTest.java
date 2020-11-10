ab33b8abb220b115ed2b2d5c97a3347e05eb76ec
LeviRoots <43782233+LeviRoots@users.noreply.github.com>
2020-11-10 19:12:52 +0000

    Initial commit

---
+++ src/test/java/Main/MainTest.java
@@ -0,0 +1,20 @@
+package Main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void mainContextLoadsAndUnloads() {
+        try (Main main = new Main()) {
+            main.init();
+        }
+    }
+
+    @Test
+    public void intentionallyFailingTest() {
+        fail("TDD Dictates you must have a failing test before you write any new code");
+    }
+}

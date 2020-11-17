0439b74e29fb693c4ad2929f45578f0b2760c800
LeviRoots <43782233+LeviRoots@users.noreply.github.com>
2020-11-17 17:32:56 +0000

    Rapid colour changing, delegated classes

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -16,111 +16,42 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class Main {
 
 	// The window handle
-	private long window;
+	private Window window;
+	private Renderer renderer;
 
 	public void run() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+		window = new Window("Dance Mat Calculator", 600, 480);
+		window.init();
+		renderer = new Renderer();
+		renderer.init();
 
-		init();
 		loop();
 
 		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
+
 
 		// Terminate GLFW and free the error callback
 		glfwTerminate();
 		glfwSetErrorCallback(null).free();
 	}
 
-	private void init() {
-		// Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
-
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Configure GLFW
-		glfwDefaultWindowHints(); // optional, the current window hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-		// Create the window
-		window = glfwCreateWindow(1200, 800, "Dance Mat Calculator", NULL, NULL);
-		if ( window == NULL )
-			throw new RuntimeException("Failed to create the GLFW window");
-        
-		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-            });
-
-		// Get the thread stack and push a new frame
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
-
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
-			);
-		} // the stack frame is popped automatically
-
-		// Make the OpenGL context current
-		glfwMakeContextCurrent(window);
-		// Enable v-sync
-		glfwSwapInterval(1);
-
-		// Make the window visible
-		glfwShowWindow(window);
-	}
-
 	private void loop() {
-		// This line is critical for LWJGL's interoperation with GLFW's
-		// OpenGL context, or any context that is managed externally.
-		// LWJGL detects the context that is current in the current thread,
-		// creates the GLCapabilities instance and makes the OpenGL
-		// bindings available for use.
-		GL.createCapabilities();
 
 		// Set the clear color
-		glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
-
+		//window.setClearColor(1.0f, 1.0f, 0.0f, 0.0f);
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
-		while ( !glfwWindowShouldClose(window) ) {
-            
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-            float[] vertex[] = 
-            {
-                {-0.5f, -0.5f},
-                 {0.5f, -0.5f},
-                 {0.0f,  0.5f}
-            };
-            glColor3f(0.0f, 0.0f, 1.0f);
-            glBegin(GL_LINE_LOOP);
-                for (int i=0; i<3;i++){
-                    glVertex2fv(vertex[i]);
-                }
-            glEnd();
-            glFlush(); 
-
-			glfwSwapBuffers(window); // swap the color buffers
-
-			// Poll for window events. The key callback above will only be
-			// invoked during this call.
-			glfwPollEvents();
+		while ( !window.windowShouldClose()) {
+			if (window.isKeyPressed(GLFW_KEY_E)) {
+				window.setClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+			} else if ( window.isKeyPressed(GLFW_KEY_DOWN) ) {
+				window.setClearColor(1.0f, 1.0f, 0.0f, 0.0f);
+			} else {
+				window.setClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+			}
+			renderer.clear();
+			window.update(); // swap the color buffers
 		}
 	}
 

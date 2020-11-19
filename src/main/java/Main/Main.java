91145997052b061a858c21a68f384fbc287b7859
LeviRoots <43782233+LeviRoots@users.noreply.github.com>
2020-11-19 16:35:29 +0000

    Basic Functionality

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -5,9 +5,48 @@ import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Main {
-	public static void main(String[] args) {
-		
-	}
+class Surface extends JPanel {
 
+    private void doDrawing(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawString("Dance Mat Calculator", 50, 50);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+}
+
+public class Main extends JFrame {
+
+    public Main() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Surface());
+
+        setTitle("Simple Java 2D example");
+        setSize(1750, 1000);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                Main ex = new Main();
+                ex.setVisible(true);
+            }
+        });
+    }
 }

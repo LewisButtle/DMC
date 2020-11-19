e6f30197dff973fe49e4bd0ce081442cb54bbef3
LeviRoots <43782233+LeviRoots@users.noreply.github.com>
2020-11-19 16:19:15 +0000

    Revert to Java 2D

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,66 +1,13 @@
 package Main;
-
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
-
-	// The window handle
-	private Window window;
-	private Renderer renderer;
-
-	public void run() {
-		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
-		window = new Window("Dance Mat Calculator", 600, 480);
-		window.init();
-		renderer = new Renderer();
-		try {
-			renderer.init();
-		}
-	 	catch (Exception excp) {
-			excp.printStackTrace();
-		}
-		loop();
-
-		// Free the window callbacks and destroy the window
-
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
-	}
-
-	private void loop() {
-
-		// Set the clear color
-		//window.setClearColor(1.0f, 1.0f, 0.0f, 0.0f);
-		// Run the rendering loop until the user has attempted to close
-		// the window or has pressed the ESCAPE key.
-		while ( !window.windowShouldClose()) {
-			if (window.isKeyPressed(GLFW_KEY_E)) {
-				window.setClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-			} else if ( window.isKeyPressed(GLFW_KEY_DOWN) ) {
-				window.setClearColor(1.0f, 1.0f, 0.0f, 0.0f);
-			} else {
-				window.setClearColor(0.0f, 0.0f, 1.0f, 0.0f);
-			}
-			renderer.render(window);
-			window.update(); // swap the color buffers
-		}
-	}
-
 	public static void main(String[] args) {
-		new Main().run();
+		
 	}
 
 }

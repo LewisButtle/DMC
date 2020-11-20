c611731bac38c8cbdc27064768b1dce0e80196de
LeviRoots <43782233+LeviRoots@users.noreply.github.com>
2020-11-20 02:14:01 +0000

    Some key functionality

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,126 +1,12 @@
 package Main;
-import java.awt.Color;
+
 import java.awt.EventQueue;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.BasicStroke;
-import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.Timer;
-
-class Surface extends JPanel implements ActionListener {
-
-    private final int DELAY = 150;
-	private Timer timer;
-
-    public Surface() {
-        initTimer();
-    }
-
-    private void initTimer() {
-
-        timer = new Timer(DELAY, this);
-        timer.start();
-    }
-    
-    public Timer getTimer() {
-        
-        return timer;
-    }
-
-    private void doDrawing(Graphics g) {
-		mainMenuScreen(g);
-
-    }
-
-	public void mainMenuScreen(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		
-        int w = getWidth();
-		int h = getHeight();
-		//right side rectangles
-		g2d.setColor(Color.YELLOW);
-		g2d.fillRect(0, 0, w, h);
-		g2d.setPaint(Color.blue);
-		g2d.fillRect(w-100, 0, 100, h);
-		g2d.setPaint(Color.green);
-		g2d.fillRect(w-200, 0, 100, h);
-		g2d.setPaint(Color.red);
-		g2d.fillRect(w-300, 0, 100, h);
-
-		BasicStroke bs1 = new BasicStroke(5, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL);
-		g2d.setStroke(bs1);
-		//centre 'buttons'
-		g2d.setColor(new Color(247, 245, 116));
-		g2d.fillRect(w/2-400, h/2-200, 800, 100);
-		g2d.fillRect(w/2-400, h/2-50, 800, 100);
-		g2d.fillRect(w/2-400, h/2+100, 800, 100);
-		g2d.fillRect(w/2-400, h/2+250, 800, 100);
-		g2d.setPaint(Color.black);
-		g2d.drawRect(w/2-400, h/2-200, 800, 100);
-		g2d.drawRect(w/2-400, h/2-50, 800, 100);
-		g2d.drawRect(w/2-400, h/2+100, 800, 100);
-		g2d.drawRect(w/2-400, h/2+250, 800, 100);
-		
-		//text
-		g2d.setPaint(new Color(186, 131, 20));
-		g2d.setFont(new Font("Ebrima Bold", Font.PLAIN, 80));
-		g2d.drawString("Dance Mat Calculator", w/2-400, h/2-310);
-		g2d.setPaint(Color.black);
-		g2d.setFont(new Font("Ebrima Bold", Font.PLAIN, 60));
-		g2d.drawString("Calculator Mode", w/2-250, h/2-125);
-		g2d.drawString("Floating Numbers", w/2-250, h/2+25);
-		g2d.drawString("Speed Numbers", w/2-250, h/2+175);
-		g2d.drawString("View Leaderboards", w/2-250, h/2+325);
-
-		g2d.setFont(new Font("Ebrima Bold", Font.PLAIN, 100));
-		g2d.setPaint(Color.red);
-		g2d.drawString("2", w/2-470, h/2-115);
-		g2d.setPaint(Color.green);
-		g2d.drawString("3", w/2-470, h/2+35);
-		g2d.setPaint(Color.blue);
-		g2d.drawString("4", w/2-470, h/2+185);
-		g2d.setPaint(Color.red);
-		g2d.drawString("5", w/2-470, h/2+335);
-		//Left diagonal rectangles
-		g2d.rotate(Math.PI/4-0.2);
-		g2d.setPaint(Color.red);
-		g2d.fillRect(0, -200, 100, 400);
-		g2d.setPaint(Color.green);
-		g2d.fillRect(100, -200, 100, 500);
-		g2d.setPaint(Color.blue);
-		g2d.fillRect(200, -200, 100, 700);
-
-		/*
-		g2d.setPaint(Color.black);
-        Random r = new Random();
-        for (int i = 0; i < 2000; i++) {
-
-            int x = Math.abs(r.nextInt()) % w;
-            int y = Math.abs(r.nextInt()) % h;
-            g2d.drawLine(x, y, x, y);
-		}
-		*/
-		g2d.dispose();
-	}
-
-    @Override
-    public void paintComponent(Graphics g) {
-
-        super.paintComponent(g);
-        doDrawing(g);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        repaint();
-    }
-}
 
 public class Main extends JFrame {
 
@@ -132,6 +18,17 @@ public class Main extends JFrame {
 		final Surface surface = new Surface();
         add(surface);
 
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keycode = e.getKeyCode();
+
+				if (keycode == 'p' || keycode == 'P') {
+					surface.screen = 1;
+					return;
+				}
+			}
+		});
 		addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -139,15 +36,14 @@ public class Main extends JFrame {
                 timer.stop();
             }
         });
-
         setTitle("Dance Mat Calculator");
         setSize(1750, 1000);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+	}
 
     public static void main(String[] args) {
-
+		
         EventQueue.invokeLater(new Runnable() {
 
             @Override
